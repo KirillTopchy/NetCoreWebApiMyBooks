03cd6e72e14f80494f5e8d606e6ba35ce95e6a3c
Kirill Topchy <kirilltopchy@gmail.com>
2022-09-25 20:50:11 +0300

    First controller added + code clean up.

---
+++ MyBooks/Controllers/BooksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
--- MyBooks/Data/AppDbContext.cs
+++ MyBooks/Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace MyBooks.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
-            :base(options)
+            : base(options)
         {
         }
 
