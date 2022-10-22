7dd6dac355738393f7dd8acbc9012a7dfc1d0c76
Kirill Topchy <kirilltopchy@gmail.com>
2022-10-22 14:15:06 +0300

    Log table created. Migration created.

--- MyBooks/Data/AppDbContext.cs
+++ MyBooks/Data/AppDbContext.cs
@@ -21,11 +21,14 @@ namespace MyBooks.Data
                 .HasOne(book => book.Author)
                 .WithMany(bookAuthors => bookAuthors.Book_Authors)
                 .HasForeignKey(bookId => bookId.AuthorId);
+
+            modelBuilder.Entity<Log>().HasKey(l => l.Id);
         }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book_Author_JoinModel> Books_Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
