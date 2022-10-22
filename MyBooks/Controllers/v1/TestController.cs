d6803fe044557114d857dc78a6ef9de8d29d5466
Kirill Topchy <kirilltopchy@gmail.com>
2022-10-22 12:54:55 +0300

    HTTP Header Based Versioning example added.

--- MyBooks/Controllers/v1/TestController.cs
+++ MyBooks/Controllers/v1/TestController.cs
@@ -3,15 +3,29 @@
 namespace MyBooks.Controllers.v1
 {
     [ApiVersion("1.0")]
-    //[Route("api/[controller]")]
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.1")]
+    [ApiVersion("1.2")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {
         [HttpGet("get-test-dataV1")]
-        public IActionResult Get()
+        public IActionResult GetV1()
         {
             return Ok("This is TestController V1");
+        }
+
+        [HttpGet("get-test-dataV1"), MapToApiVersion("1.1")]
+        public IActionResult GetV11()
+        {
+            return Ok("This is TestController V1.1");
+        }
+
+        [HttpGet("get-test-dataV1"), MapToApiVersion("1.2")]
+        public IActionResult GetV12()
+        {
+            return Ok("This is TestController V1.2");
         }
     }
 }
