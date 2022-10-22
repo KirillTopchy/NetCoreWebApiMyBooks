2cc95bbe92efff30162fa5af5d0fcf3144099e82
Kirill Topchy <kirilltopchy@gmail.com>
2022-10-22 13:00:16 +0300

    Media Type Based Versioning example added.

--- MyBooks/Startup.cs
+++ MyBooks/Startup.cs
@@ -42,7 +42,8 @@ namespace MyBooks
                 config.DefaultApiVersion = new ApiVersion(1, 0);
                 config.AssumeDefaultVersionWhenUnspecified = true;
 
-                config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+                //config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+                config.ApiVersionReader = new MediaTypeApiVersionReader();
             });
 
             services.AddSwaggerGen(c =>
