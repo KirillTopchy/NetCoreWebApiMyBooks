e167075c4e89672e5ef7f41bbc7f7db9754f1afb
Kirill Topchy <kirilltopchy@gmail.com>
2022-10-23 09:00:33 +0300

    In-Memory DataBase set up examplte

--- MyBooksTests/PublishersServiceTest.cs
+++ MyBooksTests/PublishersServiceTest.cs
@@ -1,12 +1,120 @@
+using Microsoft.EntityFrameworkCore;
+using MyBooks.Data;
+using MyBooks.Data.Models;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System;
 
 namespace MyBooksTests
 {
     public class PublishersServiceTest
     {
-        [SetUp]
+        private static DbContextOptions<AppDbContext> dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "BookDbTest")
+            .Options;
+
+        AppDbContext context;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            context = new AppDbContext(dbContextOptions);
+            context.Database.EnsureCreated();
+
+            SeedDatabase();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        private void SeedDatabase()
+        {
+            List<Publisher> publishers = new()
+            {
+                    new Publisher() {
+                        Id = 1,
+                        Name = "Publisher 1"
+                    },
+                    new Publisher() {
+                        Id = 2,
+                        Name = "Publisher 2"
+                    },
+                    new Publisher() {
+                        Id = 3,
+                        Name = "Publisher 3"
+                    },
+            };
+            context.Publishers.AddRange(publishers);
+
+            List<Author> authors = new()
+            {
+                new Author()
+                {
+                    Id = 1,
+                    FullName = "Author 1"
+                },
+                new Author()
+                {
+                    Id = 2,
+                    FullName = "Author 2"
+                }
+            };
+            context.Authors.AddRange(authors);
+
+            List<Book> books = new()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Book 1 Title",
+                    Description = "Book 1 Description",
+                    IsRead = false,
+                    Genre = "Genre",
+                    CoverUrl = "https://...",
+                    DateAdded = DateTime.Now.AddDays(-10),
+                    PublisherId = 1
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Book 2 Title",
+                    Description = "Book 2 Description",
+                    IsRead = false,
+                    Genre = "Genre",
+                    CoverUrl = "https://...",
+                    DateAdded = DateTime.Now.AddDays(-10),
+                    PublisherId = 1
+                }
+            };
+            context.Books.AddRange(books);
+
+            List<Book_Author_JoinModel> books_authors = new()
+            {
+                new Book_Author_JoinModel()
+                {
+                    Id = 1,
+                    BookId = 1,
+                    AuthorId = 1
+                },
+                new Book_Author_JoinModel()
+                {
+                    Id = 2,
+                    BookId = 1,
+                    AuthorId = 2
+                },
+                new Book_Author_JoinModel()
+                {
+                    Id = 3,
+                    BookId = 2,
+                    AuthorId = 2
+                },
+            };
+            context.Books_Authors.AddRange(books_authors);
+
+            context.SaveChanges();
         }
     }
 }
